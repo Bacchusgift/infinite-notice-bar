a42d44dbffc479d6c39d1b20b1d151433c94d07e
Bacchusgift <bacchusgift@outlook.com>
2019-07-16 18:13:52 +0800

    完成全部功能

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,77 @@
 <template>
-  <div id="app">
-
+  <div class="container">
+      <NoticeBar
+        class="notice-bar"
+        has-icon
+        bar-color="rgba(255,255,255,0.5)"
+        :notice-list="noticeList"
+        time-str=""
+      >得到</NoticeBar>
+      <div style="position: absolute;top: 50%;left: 10%;">
+        无限滚动 建议用rem做移动端适配
+      </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import NoticeBar from './infinite-notice-bar/infinite-notice-bar'
+
+  export default {
+    name: 'share',
+    components: {
+      NoticeBar
+    },
+    data () {
+      return {
+        simpleNoticeList: [
+          '你在说什么',
+          '柚子是世界上最帅的男人?'
+        ],
+        noticeList: [
+          {
+            name: '马化腾',
+            icon: 'http://youlehe-pic.yiyayouxi.com/icon/900001.jpg',
+            content: '法拉利'
+          },
+          {
+            name: '马云',
+            icon: 'http://youlehe-pic.yiyayouxi.com/icon/900001.jpg',
+            content: '太阳系'
+          }
+        ]
+      }
+    },
+    created () {
+    },
+    methods: {
+
     }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped>
+  .notice-bar {
+    position: absolute;
+    left: 5%;
+    box-shadow: #191919;
+  }
+  .container{
+    width:100%;
+    height:100vh;
+    background:rgba(254,213,190,1);
+  }
+  .background {
+    /*position: absolute;*/
+    /*background: url('../assets/share_background.png');*/
+    background-size: cover;
+    width:10rem;
+    height:18.13rem;
+    overflow: hidden;
+  }
+  body{
+    margin: 0 auto;
+  }
+  html{
+    margin: 0 auto;
+  }
 </style>
